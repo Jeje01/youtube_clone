21670429dd43017b97cee85535fe57fa62b12192
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-02-29 23:53:48 +0900

    Created mixin of video block

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+{
+    id:32453445,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+        id: 970225,
+        name:"Jaeeun",
+        email:"jaeeun@gmail.com"
+    }
+},
+{
+    id:75465,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+        id: 694949,
+        name:"Jaeeun",
+        email:"jaeeun@gmail.com"
+    }
+},
+{
+    id:6899433,
+    title: "Video amazing",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+        id: 7546345,
+        name:"Jaeeun",
+        email:"jaeeun@gmail.com"
+    }
+},
+{
+    id:8765645,
+    title: "Video good",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+        id: 9695949,
+        name:"Jaeeun",
+        email:"jaeeun@gmail.com"
+    }
+}];
+
